e35c7b663493a9d391811d3c6086ec6f3afb8cf4
abde109 <abderrahimkhadri2@gmail.com>
2024-08-16 17:41:48 +0100

    immutable

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -4,7 +4,5 @@ export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
 
-  const mergedMap = map1.mergeDeep(map2);
-
-  return mergedMap;
+  return map1.mergeDeep(map2);
 }

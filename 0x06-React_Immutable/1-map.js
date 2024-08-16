71b893f4e5eb2a957bf1392fea93a28dc22c3f5b
abde109 <abderrahimkhadri2@gmail.com>
2024-08-16 17:03:55 +0100

    immutable

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
-import {Map} from 'immutable';
+import {Map} from './node_modules/immutable/dist/immutable';
 
-export function getImmutableObject(object) {
+export default function getImmutableObject(object) {
   return Map(object);
 }
--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,7 @@
-import {fromJS} from 'immutable';
+import {fromJS} from './node_modules/immutable/dist/immutable';
 
 export default function accessImmutableObject(object, array) {
-  const immutableObject = fromJS(object);
-  return immutableObject.getIn(array);
+  const mappedObject = fromJS(object);
+
+  return mappedObject.getIn(array, undefined);
 }
